eabc181e81fe7ca52d34a7dc686f867ac016d992
Abdi Kurniawan <abdikurniawan060395@gmail.com>
2021-11-08 18:13:01 +0700

    main page layout

--- lib/main.dart
+++ lib/main.dart
@@ -10,17 +10,29 @@ void main() {
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  // final Map<int, Color> color = {
+  //   50: Color.fromRGBO(0X04, 0X48, 0X55, .1),
+  //   100: Color.fromRGBO(0X04, 0X48, 0X55, .2),
+  //   200: Color.fromRGBO(0X04, 0X48, 0X55, .3),
+  //   300: Color.fromRGBO(0X04, 0X48, 0X55, .4),
+  //   400: Color.fromRGBO(0X04, 0X48, 0X55, .5),
+  //   500: Color.fromRGBO(0X04, 0X48, 0X55, .6),
+  //   600: Color.fromRGBO(0X04, 0X48, 0X55, .7),
+  //   700: Color.fromRGBO(0X04, 0X48, 0X55, .8),
+  //   800: Color.fromRGBO(0X04, 0X48, 0X55, .9),
+  //   900: Color.fromRGBO(0X04, 0X48, 0X55, 1),
+  // };
   final Map<int, Color> color = {
-    50: Color.fromRGBO(0X04, 0X48, 0X55, .1),
-    100: Color.fromRGBO(0X04, 0X48, 0X55, .2),
-    200: Color.fromRGBO(0X04, 0X48, 0X55, .3),
-    300: Color.fromRGBO(0X04, 0X48, 0X55, .4),
-    400: Color.fromRGBO(0X04, 0X48, 0X55, .5),
-    500: Color.fromRGBO(0X04, 0X48, 0X55, .6),
-    600: Color.fromRGBO(0X04, 0X48, 0X55, .7),
-    700: Color.fromRGBO(0X04, 0X48, 0X55, .8),
-    800: Color.fromRGBO(0X04, 0X48, 0X55, .9),
-    900: Color.fromRGBO(0X04, 0X48, 0X55, 1),
+    50: Color.fromRGBO(0X00, 0X79, 0X6B, .1),
+    100: Color.fromRGBO(0X00, 0X79, 0X6B, .2),
+    200: Color.fromRGBO(0X00, 0X79, 0X6B, .3),
+    300: Color.fromRGBO(0X00, 0X79, 0X6B, .4),
+    400: Color.fromRGBO(0X00, 0X79, 0X6B, .5),
+    500: Color.fromRGBO(0X00, 0X79, 0X6B, .6),
+    600: Color.fromRGBO(0X00, 0X79, 0X6B, .7),
+    700: Color.fromRGBO(0X00, 0X79, 0X6B, .8),
+    800: Color.fromRGBO(0X00, 0X79, 0X6B, .9),
+    900: Color.fromRGBO(0X00, 0X79, 0X6B, 1),
   };
 
   @override
@@ -28,7 +40,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Aplikasi Keren',
       theme: ThemeData(
-        primarySwatch: MaterialColor(0xFF044855, color),
+        primarySwatch: MaterialColor(0xFF00796B, color),
       ),
       home: MyHomePage(),
     );
@@ -53,16 +65,16 @@ class _MyHomePageState extends State<MyHomePage> {
 
   static List<BottomNavigationBarItem> _navigationItems =
       <BottomNavigationBarItem>[
-    BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: "Beranda"),
+    BottomNavigationBarItem(icon: Icon(Icons.home), label: "Beranda"),
     BottomNavigationBarItem(
         icon: new Icon(
-          Icons.history_outlined,
+          Icons.history,
         ),
         label: "Riwayat"),
     BottomNavigationBarItem(
-        icon: new Icon(Icons.notifications_none_outlined), label: "Notifikasi"),
+        icon: new Icon(Icons.notifications_none), label: "Notifikasi"),
     BottomNavigationBarItem(
-        icon: new Icon(Icons.account_circle_outlined), label: "Profil"),
+        icon: new Icon(Icons.account_circle), label: "Profil"),
   ];
 
   void _onTabChanged(int index) {
--- lib/pages/Home/HomePage.dart
+++ lib/pages/Home/HomePage.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
-Color colorOne = Color(0xFF044855);
+// Color colorOne = Color(0xFF044855);
+Color colorOne = Colors.teal[700];
 Color colorTwo = Colors.teal;
-Color colorThree = Colors.teal[300];
+Color colorThree = Colors.teal[400];
+Color colorFour = Colors.teal[600];
 
 class HomePage extends StatelessWidget {
   HomePage({Key key}) : super(key: key);
@@ -15,111 +17,232 @@ class HomePage extends StatelessWidget {
           title: Text(title),
           elevation: 0,
         ),
-        body: Column(
+        body: Container(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            CustomPaint(
-              child: Container(
-                alignment: Alignment.topLeft,
-                width: double.infinity,
-                height: 200,
-                padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
+            MainHeader(),
+            Expanded(
+              child: GridView.count(
+                crossAxisCount: 4,
+                childAspectRatio: 1,
+                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                children: <Widget>[
+                  Column(children: [
+                    InkWell(
+                        onTap: () {},
+                        borderRadius: BorderRadius.circular(50),
+                        splashColor: Colors.teal,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            gradient: LinearGradient(
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                              colors: <Color>[
+                                Color.fromRGBO(0x00, 0x80, 0x80, .2),
+                                Color.fromRGBO(0xFF, 0xFF, 0x00, .1)
+                              ],
+                            ),
+                          ),
+                          child: SizedBox(
+                            width: 50,
+                            height: 50,
+                            child: Icon(Icons.phone_android,
+                                color: Color(0xFF044855)),
+                          ),
+                        )),
+                    SizedBox(height: 5),
+                    Text(
+                      "Pulsa",
+                      style: TextStyle(fontSize: 12),
+                    )
+                  ]),
+                  Column(children: [
+                    InkWell(
+                        onTap: () {},
+                        borderRadius: BorderRadius.circular(50),
+                        splashColor: Colors.teal,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            gradient: LinearGradient(
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                              colors: <Color>[
+                                Color.fromRGBO(0x00, 0x80, 0x80, .2),
+                                Color.fromRGBO(0xFF, 0xFF, 0x00, .1)
+                              ],
+                            ),
+                          ),
+                          child: SizedBox(
+                            width: 50,
+                            height: 50,
+                            child: Icon(Icons.bolt, color: Color(0xFF044855)),
+                          ),
+                        )),
+                    SizedBox(height: 5),
+                    Text(
+                      "PLN",
+                      style: TextStyle(fontSize: 12),
+                    )
+                  ]),
+                  Column(children: [Icon(Icons.sports_esports), Text("Item")]),
+                  Column(children: [Icon(Icons.public), Text("Item")]),
+                  Column(children: [Icon(Icons.tv), Text("Item")]),
+                  Column(
+                      children: [Icon(Icons.water_damage_sharp), Text("Item")]),
+                  Column(children: [Icon(Icons.verified_user), Text("Item")]),
+                  Column(children: [Icon(Icons.apps), Text("Item")]),
+                ],
+              ),
+            ),
+          ],
+        )));
+  }
+}
+
+class MainHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      child: Container(
+        alignment: Alignment.topLeft,
+        width: double.infinity,
+        height: 160,
+        padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Row(children: [
+              Text(
+                "Cash",
+                style: TextStyle(color: Colors.white),
+              ),
+            ]),
+            SizedBox(height: 10),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "Rp",
+                  style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+                Text("1.234.567",
+                    style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white)),
+              ],
+            ),
+            Card(
+              margin: EdgeInsets.fromLTRB(10, 20, 10, 0),
+              clipBehavior: Clip.antiAlias,
+              color: Colors.white,
+              elevation: 3.0,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 35.0, vertical: 13.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
-                    Row(children: [
-                      Text(
-                        "Cash",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ]),
-                    SizedBox(height: 10),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          "Rp",
-                          style: TextStyle(
-                              fontSize: 12,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white),
+                    Column(
+                      children: [
+                        Icon(
+                          Icons.add_box,
+                          color: Color(0xFF044855),
                         ),
-                        Text("1.234.567",
-                            style: TextStyle(
-                                fontSize: 24,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white)),
+                        Text(
+                          "Top Up",
+                          style:
+                              TextStyle(color: Color(0xFF044855), fontSize: 12),
+                        )
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Icon(
+                          Icons.swap_horiz,
+                          color: Color(0xFF044855),
+                        ),
+                        Text(
+                          "Transfer",
+                          style:
+                              TextStyle(color: Color(0xFF044855), fontSize: 12),
+                        )
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Icon(
+                          Icons.pending_actions,
+                          color: Color(0xFF044855),
+                        ),
+                        Text(
+                          "Mutasi",
+                          style:
+                              TextStyle(color: Color(0xFF044855), fontSize: 12),
+                        )
                       ],
                     ),
                   ],
                 ),
               ),
-              painter: CurvePainter(),
             ),
-            Card(
-              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-              clipBehavior: Clip.antiAlias,
-              elevation: 5,
-            )
           ],
-        ));
+        ),
+      ),
+      painter: HeaderBackground(),
+    );
   }
 }
 
-class CurvePainter extends CustomPainter {
+class HeaderBackground extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     Path path = Path();
     Paint paint = Paint();
 
-    path.lineTo(0, size.height * 0.75);
-    path.quadraticBezierTo(size.width * 0.10, size.height * 0.70,
-        size.width * 0.17, size.height * 0.90);
+    path.lineTo(0, size.height * 0.65);
+    path.quadraticBezierTo(size.width * 0.20, size.height * 0.90,
+        size.width * 0.5, size.height * 0.90);
     path.quadraticBezierTo(
-        size.width * 0.20, size.height, size.width * 0.25, size.height * 0.90);
-    path.quadraticBezierTo(size.width * 0.40, size.height * 0.40,
-        size.width * 0.50, size.height * 0.70);
-    path.quadraticBezierTo(size.width * 0.60, size.height * 0.85,
-        size.width * 0.65, size.height * 0.65);
-    path.quadraticBezierTo(
-        size.width * 0.70, size.height * 0.90, size.width, 0);
-    path.close();
-
-    paint.color = colorThree;
-    canvas.drawPath(path, paint);
-
-    path = Path();
-    path.lineTo(0, size.height * 0.50);
-    path.quadraticBezierTo(size.width * 0.10, size.height * 0.80,
-        size.width * 0.15, size.height * 0.60);
-    path.quadraticBezierTo(size.width * 0.20, size.height * 0.45,
-        size.width * 0.27, size.height * 0.60);
-    path.quadraticBezierTo(
-        size.width * 0.45, size.height, size.width * 0.50, size.height * 0.80);
-    path.quadraticBezierTo(size.width * 0.55, size.height * 0.45,
-        size.width * 0.75, size.height * 0.75);
-    path.quadraticBezierTo(
-        size.width * 0.85, size.height * 0.93, size.width, size.height * 0.60);
+        size.width * 0.80, size.height * 0.90, size.width, size.height * 0.65);
     path.lineTo(size.width, 0);
-    path.close();
-
     paint.color = colorTwo;
     canvas.drawPath(path, paint);
 
     path = Path();
-    path.lineTo(0, size.height * 0.75);
-    path.quadraticBezierTo(size.width * 0.10, size.height * 0.55,
-        size.width * 0.22, size.height * 0.60);
-    path.quadraticBezierTo(size.width * 0.30, size.height * 0.90,
-        size.width * 0.40, size.height * 0.75);
-    path.quadraticBezierTo(size.width * 0.52, size.height * 0.50,
-        size.width * 0.65, size.height * 0.70);
-    path.quadraticBezierTo(
-        size.width * 0.75, size.height * 0.85, size.width, size.height * 0.60);
-    path.lineTo(size.width, 0);
-    path.close();
+    path.lineTo(0, size.height * 0.15);
+    path.quadraticBezierTo(size.width * 0.30, size.height * 0.50,
+        size.width * 0.60, size.height * 0.20);
+    paint.color = colorThree;
+    canvas.drawPath(path, paint);
 
+    path = Path();
+    path.lineTo(0, size.height * 0.05);
+    path.quadraticBezierTo(size.width * 0.30, size.height * 0.40,
+        size.width * 0.60, size.height * 0.20);
+    path.quadraticBezierTo(
+        size.width * 0.80, size.height * 0.05, size.width, size.height * 0.20);
+    path.lineTo(size.width, 0);
     paint.color = colorOne;
+    canvas.drawPath(path, paint);
+
+    path = Path();
+    path.moveTo(size.width, size.height * 0.35);
+    path.lineTo(size.width, size.height * 0.20);
+    path.quadraticBezierTo(size.width * 0.85, size.height * 0,
+        size.width * 0.30, size.height * 0.75);
+    path.quadraticBezierTo(
+        size.width * 0.80, size.height * 0.15, size.width, size.height * 0.35);
+
+    path.lineTo(size.width, size.height * .35);
+    paint.color = colorFour;
     canvas.drawPath(path, paint);
   }
 
